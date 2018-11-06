3f229cfcd6111f93aa3f192a2cdfa27feac9c771
Matt Magin <matt.magin@cmv.com.au>
2018-11-06 11:22:45 +1030

    Make compatible with MIDDLEWARE_CLASSES

--- aws_healthchecker_response/middleware.py
+++ aws_healthchecker_response/middleware.py
@@ -1,14 +1,13 @@
 from django.conf import settings
 from django.http.response import HttpResponse
+from django.utils.deprecation import MiddlewareMixin
 
 DEFAULT_USER_AGENT = ("ELB-HealthChecker/2.0",)
 
 
-class AWSHealthCheckerResponseMiddleware:
-    def __init__(self, get_response):
-        self.get_response = get_response
+class AWSHealthCheckerResponseMiddleware(MiddlewareMixin):
 
-    def __call__(self, request):
+    def process_request(self, request):
         if request.META.get('User-Agent', None) in getattr(settings, 'AWS_HEALTH_CHECKER_USER_AGENT', DEFAULT_USER_AGENT):
             return HttpResponse("OK")
         return self.get_response(request)
